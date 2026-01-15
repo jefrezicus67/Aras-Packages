5f32eae5f98f9558e2ad0e4b42e0fa7f5422c3f4
jefrezicus67 <122387070+jefrezicus67@users.noreply.github.com>
2026-01-15 11:02:28 -0700

    Register ConvertTextToChapterAction in ActionsHelper

---
+++ ActionsHelper.ts
@@ -0,0 +1,7 @@
+//THIS IS MODIFICATION TO THE REFERENCED FILE, NOT DIRECT USE:
+import { ConvertTextToChapterAction } from './Actions/ConvertTextToChapterAction';
+
+// In the action registration section:
+this._actions['converttexttochapter'] = new ConvertTextToChapterAction({
+    actionsHelper: this
+});
